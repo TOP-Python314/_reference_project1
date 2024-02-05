6d4095a3e4f7920a41801786d6b68b110c80ed3f
Gennadiy <renardes@gmail.com>
2024-02-05 12:44:56 +0500

    main.config(): чтение файлов

--- src/data.py
+++ src/data.py
@@ -2,3 +2,28 @@
 Вспомогательный модуль — глобальные переменные и условные константы.
 """
 
+# стандартная библиотека
+from pathlib import Path
+from sys import path, argv
+from typing import Any
+
+
+# переменные для аннотаций
+Players = dict[str, dict[str, int]]
+Saves = dict[frozenset[str, str], dict[str, Any]]
+
+
+# корень проекта
+ROOT_DIR = Path(path[0]).parent
+# каталог данных
+DATA_DIR = ROOT_DIR / 'data'
+# пути к файлам данных
+players_path = DATA_DIR / 'players.ini'
+saves_path = DATA_DIR / 'saves.ttt'
+
+
+# словарь игроков
+players_db: Players = {}
+# словарь сохранений
+saves_db: Saves = {}
+
--- src/main.py
+++ src/main.py
@@ -2,9 +2,17 @@
 Точка входа. Управляющий код верхнего уровня
 """
 
+# текущий проект
+import data
+import files
+import utils
+
 
 def config():
     """Чтение файлов, инициализация структур данных."""
+    data.players_db = files.read_players()
+    data.saves_db = files.read_saves()
+    
     ...
 
 
