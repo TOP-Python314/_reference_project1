fc952987e37c2dbc8c058b779fbc8fbaaefdb27f
Gennadiy <renardes@gmail.com>
2024-02-05 18:49:40 +0500

    сигнатуры основных исполнительных функций и дополнительные глобальные переменные

--- src/help.py
+++ src/help.py
@@ -2,6 +2,9 @@
 Исполнительный модуль — раздел помощи.
 """
 
+# текущий проект
+import data
+
 
 def print_full_help() -> None:
     """Отправляет в stdout полную справку."""
@@ -21,6 +24,13 @@ def render_game_info() -> str:
 def render_interface_help() -> str:
     """Возвращает строку с описанием интерфейса командной строки."""
     ...
+
+
+def render_numerated_filed(center: bool = True) -> str:
+    """Возвращает строку игрового поля с номерами клеток.
+    
+    :param center: выравнивание: True - по центру, False - влево
+    """
 
 
 def render_commands() -> str:
